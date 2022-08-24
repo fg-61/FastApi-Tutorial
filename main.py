279eda1d96bc469e054b4142dc45a989d2e01085
Fatih Gören <fatihgoren61@gmail.com>
2022-08-24 18:50:07 +0300

    basic get requests

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ def index():
 @app.get('/about')
 def about():
     return {'data':'about page'}
+
+# unpublished bir aşağıda olsaydı path çakışması olacaktı
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
