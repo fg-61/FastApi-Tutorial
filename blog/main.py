8f5d1e88d194ca7384fc8c64805582703ce2c29c
Fatih Gören <fatihgoren61@gmail.com>
2022-08-30 15:44:00 +0300

    response-model

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas
 from . import schemas, models
@@ -25,13 +26,13 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def get_by_id(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -57,7 +58,8 @@ def delete_by_id(id, db: Session = Depends(get_db)):
 def update_by_id(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id:{id} not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Blog with id:{id} not found")
 
     blog.update(request.dict())
     db.commit()
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,12 @@
+from pickle import TRUE
 from pydantic import BaseModel
 
 class Blog(BaseModel):
     title: str
     body: str
     
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    class Config():
+        orm_mode = True
