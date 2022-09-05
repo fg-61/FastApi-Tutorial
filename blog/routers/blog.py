e909ff528c626a2735841293545be13e331509e2
Fatih Gören <fatihgoren61@gmail.com>
2022-09-05 23:34:36 +0300

    router -> tags

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -4,19 +4,21 @@ from .. import schemas, models, database
 from sqlalchemy.orm import Session
 
 
-router = APIRouter()
+router = APIRouter(
+    tags=['blogs']
+)
 get_db = database.get_db
 
 
 
 
-@router.get('/blog', response_model=List[schemas.ShowBlog], tags=["blogs"])
+@router.get('/blog', response_model=List[schemas.ShowBlog])
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.post('/blog', status_code=status.HTTP_201_CREATED, tags=["blogs"])
+@router.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
@@ -25,7 +27,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@router.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=["blogs"])
+@router.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def get_by_id(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -34,7 +36,7 @@ def get_by_id(id, db: Session = Depends(get_db)):
     return blog
 
 
-@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"])
+@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_by_id(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)(synchronize_session=False)
     if not blog.first():
@@ -45,7 +47,7 @@ def delete_by_id(id, db: Session = Depends(get_db)):
     return 'done'
 
 
-@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
+@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update_by_id(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
